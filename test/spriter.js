c8df08bf7d59b9b8f8de2038be0a2ff127a4c0f8
Simen Brekken <simen@unfold.no>
2012-11-05 16:09:30 +0100

    Add tests.

---
+++ test/spriter.js
@@ -0,0 +1,28 @@
+var spriter = require('..'),
+    fs = require('fs'),
+    Canvas = require('canvas');
+
+function fixture(name) {
+    return fs.readFileSync('test/fixtures/' + name + '.css', 'utf8');
+}
+
+describe('spriter', function() {
+    var targetPath = 'images/generated/sprites';
+    var sourcePath = 'images/sprites';
+
+    it('should generate sprite sheet', function() {
+        spriter('test/fixtures/simple.css', sourcePath, targetPath).should.equal(fixture('simple.out'));
+    });
+
+    it('should read declarations within media queries', function() {
+        spriter('test/fixtures/media-query.css', sourcePath, targetPath).should.equal(fixture('media-query.out'));
+    });
+
+    it('should not read filtered declarations', function() {
+       spriter('test/fixtures/filtered.css', sourcePath, targetPath).should.equal(fixture('filtered.out'));
+    });
+
+    it('should group sprites by suffix', function() {
+        spriter('test/fixtures/suffix.css', sourcePath, targetPath).should.equal(fixture('suffix.out'));
+    });
+});
